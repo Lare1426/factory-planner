20d4ebfb25257fdc9d3d019b715953b568258b94
Lare1426 <laurikj.virta@gmail.com>
2023-05-19 15:17:20 +0300

    install react router and set up simple navigation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+import { Route, Routes, Link } from "react-router-dom";
 import "./App.module.scss";
+import { BookList, Home } from "@/components";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/books">Books</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books" element={<BookList />} />
+      </Routes>
+    </>
+  );
 }
---
+++ client/src/components/index.jsx
@@ -0,0 +1,4 @@
+import BookList from "./BookList";
+import Home from "./Home";
+
+export { BookList, Home };
