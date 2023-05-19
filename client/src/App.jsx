92730e34f4d7eacf5b7d22695e5c5c5a5cba71dd
Lare1426 <laurikj.virta@gmail.com>
2023-05-19 14:32:27 +0300

    initialise express for server and vite, sass and css modules for client

---
+++ client/src/App.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./App.module.scss";
+
+export default function App() {
+  return <div>App</div>;
+}
---
+++ server/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const PORT = process.env.PORT ?? 3000;
+const IP = process.env.IP;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const server = express();
+
+if (process.env.NODE_ENV === "production") {
+  server.use(express.static(path.join(__dirname, "../client")));
+}
+
+server.listen(PORT, IP, () => {
+  console.log("Listening to port", PORT);
+});
