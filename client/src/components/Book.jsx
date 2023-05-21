8e7a19f4bc0d39a95b4a22f7636df17881a65ec8
Lare1426 <laurikj.virta@gmail.com>
2023-05-21 12:16:00 +0300

    disable button after first click

--- client/src/components/Book.jsx
+++ client/src/components/Book.jsx
@@ -1,12 +1,32 @@
-import { useParams, useOutletContext } from "react-router-dom";
+import { useParams, useOutletContext, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function Book() {
   const { id } = useParams();
   const context = useOutletContext();
+  const navigate = useNavigate();
+  const [isButtonActive, setIsButtonActive] = useState(false);
+
+  const sleep = (time) =>
+    new Promise((resolve) => {
+      setTimeout(resolve, time);
+    });
 
   return (
-    <h1>
-      Book {id} {context.hello}
-    </h1>
+    <>
+      <h1>
+        Book {id} {context.hello}
+      </h1>
+      <button
+        disabled={isButtonActive}
+        onClick={async () => {
+          setIsButtonActive(true);
+          await sleep(2000);
+          navigate("../");
+        }}
+      >
+        Back to books
+      </button>
+    </>
   );
 }
