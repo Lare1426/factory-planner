282a8a7a284d591cd8e7cd91f1ce859a388a1773
Lare1426 <laurikj.virta@gmail.com>
2023-08-02 15:22:32 +0300

    add routing

---
+++ client/src/routes/Home.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import styles from "./Home.module.scss";
+import { Button, Input } from "@/components";
+
+export default function Home() {
+  const [product, setProduct] = useState("");
+  const [amount, setAmount] = useState("");
+  let isGeneratePlanDisabled = !(product && amount);
+
+  return (
+    <main className={styles.home}>
+      <div className={styles.planList}>
+        <h2>Top 10 most viewed plans</h2>
+        <hr />
+      </div>
+      <div className={styles.rightContainer}>
+        <div className={styles.buttons}>
+          <Button size="large" color="primary" shadow="drop">
+            View
+          </Button>
+          <Button size="large" color="primary" shadow="drop">
+            Create
+          </Button>
+        </div>
+        <div className={styles.quickCreate}>
+          <h2>Quick create</h2>
+          <Input
+            type="text"
+            placeholder="Select product"
+            size="large"
+            setValue={setProduct}
+            value={product}
+          />
+          <label>Total amount per minute</label>
+          <Input
+            type="number"
+            placeholder="0"
+            size="small"
+            min={0}
+            max={20000}
+            setValue={setAmount}
+            value={amount}
+          />
+          <Button
+            size="small"
+            color="primary"
+            disabled={isGeneratePlanDisabled}
+          >
+            Generate plan
+          </Button>
+        </div>
+      </div>
+    </main>
+  );
+}
