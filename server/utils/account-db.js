56f21d328881170cd08ce2ec542cc4312e85356e
Lare1426 <laurikj.virta@gmail.com>
2023-12-09 19:26:58 +0200

    add account db utility

---
+++ server/utils/account-db.js
@@ -0,0 +1,61 @@
+import mysql from "mysql2/promise";
+
+const connectionConfig = {
+  database: "lare_factory-planner",
+  host: "mysql-lare.alwaysdata.net",
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+};
+
+const executeQuery = async (queryString, queryParams) => {
+  const connection = await mysql.createConnection(connectionConfig);
+  const result = await connection.execute(queryString, queryParams);
+  connection.end();
+  return result;
+};
+
+export const insertAccount = async ({ id, username, password }) => {
+  const [result] = await executeQuery(
+    "INSERT INTO account (id, username, password) VALUES (?, ?, ?);",
+    [id, username, password]
+  );
+  return result;
+};
+
+export const deleteAccount = async ({ id }) => {
+  const [result] = await executeQuery("DELETE FROM account WHERE id = ?;", [
+    id,
+  ]);
+  return result;
+};
+
+export const updateAccount = async ({ id, ...values }) => {
+  const updateFields = [];
+  const updateValues = [];
+
+  for (const key of ["username", "password"]) {
+    if (values[key]) {
+      updateFields.push(`${key} = ?`);
+      updateValues.push(values[key]);
+    }
+  }
+
+  const [result] = await executeQuery(
+    `UPDATE account SET ${updateFields.join(",")} WHERE id = ?;`,
+    [...updateValues, id]
+  );
+  return result;
+};
+
+export const selectAccount = async ({ id, username }) => {
+  const [[account]] = await executeQuery(
+    `SELECT * FROM account WHERE ${id ? "id" : "username"} = ?`,
+    [id ?? username]
+  );
+  return account;
+};
+
+export const selectAccounts = async () => {
+  const [rows] = await executeQuery("SELECT * FROM account;");
+  return rows;
+};
