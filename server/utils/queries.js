b7238cf521bf37136717799a7caee07f8774b3b1
Lare1426 <laurikj.virta@gmail.com>
2024-07-07 18:04:46 +0300

    Query formatting

--- server/utils/queries.js
+++ server/utils/queries.js
@@ -3,22 +3,31 @@ import { executeQuery } from "./rdb.js";
 export const selectAccountPlans = async (accountId) => {
   let [rdbResult] = await executeQuery(
     `
-    SELECT plan.name, plan.description, plan.id, plan.product, plan.amount, plan.isPublic, plan.creator, account_plan.shared, account_plan.favourited, account_plan.created
+    SELECT 
+      plan.name,
+      plan.description,
+      plan.id,
+      plan.product,
+      plan.amount,
+      plan.isPublic,
+      plan.creator,
+      account_plan.shared,
+      account_plan.favourited,
+      account_plan.created
     FROM account_plan 
-    INNER JOIN plan
-    ON account_plan.planId=plan.id
-    WHERE account_plan.accountId=?;`,
+    INNER JOIN plan 
+      ON account_plan.planId=plan.id
+    WHERE account_plan.accountId=?;
+    `,
     [accountId]
   );
 
-  rdbResult = rdbResult.map((row) => {
+  return rdbResult.map((row) => {
     row.shared = !!row.shared;
     row.favourited = !!row.favourited;
     row.created = !!row.created;
     return row;
   });
-
-  return rdbResult;
 };
 
 export const selectSharedPlans = async (accountId) => {
@@ -27,7 +36,7 @@ export const selectSharedPlans = async (accountId) => {
     SELECT B.planId, account.username
     FROM account_plan AS A, account_plan AS B
     INNER JOIN account
-    ON B.accountId=account.id
+      ON B.accountId=account.id
     WHERE A.accountId=? AND A.created=1 AND B.planId=A.planId AND B.shared=1;
     `,
     [accountId]
@@ -46,11 +55,16 @@ export const selectSharedPlans = async (accountId) => {
 export const selectPlanMetadata = async (accountId, planId) => {
   const [[rdbResult]] = await executeQuery(
     `
-    SELECT account_plan.shared, account_plan.created, account_plan.favourited, plan.isPublic
+    SELECT 
+      account_plan.shared, 
+      account_plan.created, 
+      account_plan.favourited, 
+      plan.isPublic
     FROM plan
     INNER JOIN account_plan
-    ON plan.id=account_plan.planId
-    WHERE account_plan.accountId=? AND account_plan.planId=?;`,
+      ON plan.id=account_plan.planId
+    WHERE account_plan.accountId=? AND account_plan.planId=?;
+    `,
     [accountId, planId]
   );
 
